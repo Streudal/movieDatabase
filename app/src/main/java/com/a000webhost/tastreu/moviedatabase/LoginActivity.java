db098ee681510dca26cfdc26c6f173ff56dd9627
tastreu <tylerstreu81@gmail.com>
2018-11-30 10:29:04 -0500

    fix mixing features

--- app/src/main/java/com/a000webhost/tastreu/moviedatabase/LoginActivity.java
+++ app/src/main/java/com/a000webhost/tastreu/moviedatabase/LoginActivity.java
@@ -2,12 +2,16 @@ package com.a000webhost.tastreu.moviedatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login_screen);
+        view = this.getWindow().getDecorView();
+        view.setBackgroundResource(R.color.lightBlue);
     }
 }
